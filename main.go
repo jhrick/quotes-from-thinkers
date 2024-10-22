70176a193790ab1a72641abe683bdbbf9a4db031
Jorge <hj.rickcp@gmail.com>
2024-10-22 13:18:10 -0300

    fix: remove unnecessary code

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"os"
-
-	"github.com/jhrick/quotes-from-thinkers/internal/services"
-)
-
+import "github.com/jhrick/quotes-from-thinkers/internal/services"
 
 func main() {
   quoteChannel := make(chan services.QuoteSchema)
@@ -20,11 +14,4 @@ func main() {
   go scrapperService.GetData(subdirectory, 2)
 
   quoteService.GetQuotes()
-  
-  stdout := json.NewEncoder(os.Stdout)
-  stdout.SetIndent("", "  ")
-
-  for quote := range quoteService.QuoteChannel {
-    stdout.Encode(services.QuoteSchema{Author: quote.Author, Text: quote.Text})
-  }
 }
